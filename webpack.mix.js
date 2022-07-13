26039ed4c61072b69dae29920cff1ee2641bada7
andrew <andrews.mr@yandex.ru>
2022-07-13 14:28:17 +0300

    init frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.copy('quasarapp/dist/spa/index.html', 'resources/views/app.blade.php')
+    .copyDirectory('quasarapp/dist/spa', 'public');
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
